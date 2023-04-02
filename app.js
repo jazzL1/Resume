1915c0ea825eca811277e18c16820f28b49a6c12
jLeibur <jazz_leibur@hotmail.com>
2023-04-01 21:10:37 -0400

    testing static files

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.set("public", path.join(__dirname, "public"));
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
 
